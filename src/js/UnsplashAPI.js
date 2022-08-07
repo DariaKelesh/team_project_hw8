9ac6e380610b2bda519d136a2137bddaa351e976
Elena-Holz <97180066+Elena-Holz@users.noreply.github.com>
2022-08-07 12:55:46 +0300

    addend

--- src/js/UnsplashAPI.js
+++ src/js/UnsplashAPI.js
@@ -2,14 +2,10 @@ export class UnsplashAPI {
   #BASE_URL = 'https://api.unsplash.com/search/photos';
   #API_KEY = 'LxvKVGJqiSe6NcEVZOaLXC-f2JIIWZaq_o0WrF8mwJc';
   #query = '';
+  #searchParams = new URLSearchParams({per_page: 10, client_id:this.#API_KEY, color:'blue', orientation:'portrait'})
 
   getPopularImages(page) {
-    const url = `${
-      this.#BASE_URL
-    }?page=${page}&query=office&per_page=10&color=blue&orientation=portrait&client_id=${
-      this.#API_KEY
-    }`;
-
+    const url = `${this.#BASE_URL}?page=${page}&query=office&${this.#searchParams}`;
     return fetch(url).then(response => {
       if (!response.ok) {
         throw new Error(response.status);
@@ -21,7 +17,7 @@ export class UnsplashAPI {
   getImagesBuyQuery(page) {
     const url = `${this.#BASE_URL}?page=${page}&query=${
       this.#query
-    }&per_page=10&color=blue&orientation=portrait&client_id=${this.#API_KEY}`;
+    }&${this.#searchParams}`;
 
     return fetch(url).then(response => {
       if (!response.ok) {
