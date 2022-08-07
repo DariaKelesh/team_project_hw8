31dd0dee50dec61d6cc43a67a2d391a9899e327e
Nikita <nicstyle22@gmail.com>
2022-08-07 11:58:56 +0300

    add gallery-2

--- src/js/UnsplashAPI.js
+++ src/js/UnsplashAPI.js
@@ -1,6 +1,8 @@
 export class UnsplashAPI {
   #BASE_URL = 'https://api.unsplash.com/search/photos';
   #API_KEY = 'LxvKVGJqiSe6NcEVZOaLXC-f2JIIWZaq_o0WrF8mwJc';
+  #query = '';
+
   getPopularImages(page) {
     const url = `${
       this.#BASE_URL
@@ -14,5 +16,22 @@ export class UnsplashAPI {
       }
       return response.json();
     });
+  }
+
+  getImagesBuyQuery(page) {
+    const url = `${this.#BASE_URL}?page=${page}&query=${
+      this.#query
+    }&per_page=10&color=blue&orientation=portrait&client_id=${this.#API_KEY}`;
+
+    return fetch(url).then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    });
+  }
+
+  set query(newQuery) {
+    this.#query = newQuery;
   }
 }
